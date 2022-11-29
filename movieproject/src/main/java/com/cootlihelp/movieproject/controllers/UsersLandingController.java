a47acf8c2d08e1cb8c594eb6d78700175b5e5228
Hector Cosgalla <valcos98@hotmail.com>
2022-11-29 17:25:26 -0600

    Update algo

--- movieproject/src/main/java/com/cootlihelp/movieproject/controllers/UsersLandingController.java
+++ movieproject/src/main/java/com/cootlihelp/movieproject/controllers/UsersLandingController.java
@@ -5,18 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.bytebuddy.build.Plugin.Engine.ErrorHandler;
 
 @RestController
 @RequestMapping("")
 /**
  * TestController
  **/
-public class TestController {
+public class UsersLandingController {
 
-    @GetMapping("/")
+    @GetMapping("/user")
 
     public ResponseEntity<String> get() {
-        return ResponseEntity.ok("Welcome to the jungle B*TCH!");
+        return AppErrorController.class;
     }
 }
-
---
+++ movieproject/src/main/java/com/cootlihelp/movieproject/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.cootlihelp.movieproject.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.cootlihelp.movieproject.models.WebsiteUser;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<WebsiteUser, Long> {
+    List<WebsiteUser> findByName(@Param("name") String name);
+}
